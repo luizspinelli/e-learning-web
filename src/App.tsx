3ea0d55ec2731a714b0cbb28d57df62d25948db0
Luiz Spinelli <luiz.lucchi@gmail.com>
2021-01-30 22:39:36 -0300

    Global Style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import GlobalStyle from './styles/index';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div>
-      <div>E-Learning</div>
-    </div>
+    <>
+      <GlobalStyle />
+    </>
   );
-}
+};
 
 export default App;
